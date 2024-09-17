6280c26909e7dc71dbe27cbf57ae7ac5dc1e7eb9
anas <anasint@hotmail.com>
2024-09-17 15:58:59 -0400

    the code adds and removes buddy

---
+++ src/AddressBook.java
@@ -0,0 +1,30 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class AddressBook {
+    private List<BuddyInfo> buddies;
+
+    public AddressBook() {
+        buddies = new ArrayList<>();
+    }
+
+    public void addBuddy(BuddyInfo buddy) {
+        if (buddy != null) {
+            buddies.add(buddy);
+            System.out.println(buddy.getName() + " added to AddressBook");
+        }
+    }
+
+    public void removeBuddy(BuddyInfo buddy) {
+        if (buddies.remove(buddy)) {
+            System.out.println(buddy.getName() + " removed from AddressBook");
+        }
+    }
+
+    public static void main(String[] args) {
+        AddressBook addressBook = new AddressBook();
+        BuddyInfo buddy = new BuddyInfo("John", "904 Ralph Henesy", "123");
+        addressBook.addBuddy(buddy);
+        addressBook.removeBuddy(buddy);
+    }
+}
